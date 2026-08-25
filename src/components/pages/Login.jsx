49cbbe8a7332ac0984bcaa86fc5a80ae482e2293
luigiIaco <157249321+luigiIaco@users.noreply.github.com>
2026-08-25 17:52:54 +0200

    Update Login.jsx

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -74,7 +74,7 @@ const Form = () => {
         Cookies.remove("rememberCredential");
       }
 
-      navigate("/home");
+      window.location.href = "/home";
     } catch (err) {
       setError("Credenziali non valide. Riprova.");
     } finally {
